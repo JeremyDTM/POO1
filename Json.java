532c9283154a108e408e187d72e0591a8edd7db0
CarlosVNZ <43708929+CarlosVNZ@users.noreply.github.com>
2018-09-30 19:06:15 -0600

    Update Json.java

--- Json.java
+++ Json.java
@@ -16,40 +16,74 @@ public class Json {
     JSONArray arregloJson = (JSONArray) parser.parse(new FileReader("C:\\Users\\Carlos Vega\\Desktop\\Progra\\leer.json"));
     for (int i =0; i<arregloJson.size();i++){
       JSONObject elemento = (JSONObject) arregloJson.get(i);
-     // if(buscarPlaca(elemento.get("placa").toString())){
-        Vehiculo vehiculo;
-        //vehiculo = new Vehiculo(elemento.get("placa").toString(),elemento.get("anio").toString(),elemento.get("color").toString(),
-        //elemento.get("marca").toString(),Integer.parseInt(elemento.get("capacidad").toString()),
-        //elemento.get("sede").toString(),elemento.get("estado").toString(),
-        //Integer.parseInt(elemento.get("vin").toString()));
-       // vehiculos.add(vehiculo);
-     // }
+      Vehiculo vehiculo = new Vehiculo(elemento.get("placa").toString(),elemento.get("anio").toString(),elemento.get("color").toString(),
+      elemento.get("marca").toString(),Integer.parseInt(elemento.get("capacidad").toString()),elemento.get("sede").toString(),
+      elemento.get("estado").toString(),Integer.parseInt(elemento.get("vin").toString()));
+      vehiculos.add(vehiculo);
+      
     }
   }
   public void escribirJson(){  
+    
     try (FileWriter file = new FileWriter("C:\\Users\\Carlos Vega\\Desktop\\Progra\\leer.json")) {
       JSONObject obj = new JSONObject();
       JSONArray company = new JSONArray();
-      
+
       obj.put("Name", "Prueba" );
-      
+
       obj.put("hola","mundo" );
       obj.put("c++", "python" );
       obj.put("Author", "Vega" );
-      
-      company.add(obj);
-      
 
-      
+      company.add(obj);
+
+
+
       JSONObject wy = new JSONObject();
       wy.put("que", "weon");
       company.add(wy);
-      file.write(company.toJSONString());
+      file.write(company.toJSONString()+"\n");
       file.flush();
+      file.close();
       System.out.println("Successfully Copied JSON Object to File...");
       System.out.println("\nJSON Object: " + obj);
     }catch(IOException e){
-        
+
+    }
+    
+  }
+  public void leerJsonChofer() throws FileNotFoundException,IOException,ParseException{
+    JSONParser parser= new JSONParser();
+    JSONArray arregloJson = (JSONArray) parser.parse(new FileReader("C:\\Users\\Carlos Vega\\Desktop\\Progra\\leer.json"));
+    for (int i =0; i<arregloJson.size();i++){
+      JSONObject elemento = (JSONObject) arregloJson.get(i);
+      Chofer chofer= new Chofer(elemento.get("nombre").toString(),Integer.parseInt(elemento.get("capacidad").toString()),
+      elemento.get("correo").toString(),Integer.parseInt(elemento.get("capacidad").toString()));
+      choferes.add(chofer);
+              
+    }
+    
+  }
+  public void leerJsonPasajeros() throws FileNotFoundException,IOException,ParseException{
+    JSONParser parser= new JSONParser();
+    JSONArray arregloJson = (JSONArray) parser.parse(new FileReader("C:\\Users\\Carlos Vega\\Desktop\\Progra\\Pasajeros.json"));
+    for (int i =0; i<arregloJson.size();i++){
+      JSONObject elemento = (JSONObject) arregloJson.get(i);
+     // if(buscarPlaca(elemento.get("placa").toString())){
+        Pasajero pasajero= new Pasajero(elemento.get("nombre").toString(),Integer.parseInt(elemento.get("cedula").toString()),
+        elemento.get("correo").toString(),Integer.parseInt(elemento.get("telefono").toString()));
+        pasajeros.add(pasajero);
+    }
+  }
+  public void leerJsonUsuarios() throws FileNotFoundException,IOException,ParseException{
+    JSONParser parser= new JSONParser();
+    JSONArray arregloJson = (JSONArray) parser.parse(new FileReader("C:\\Users\\Carlos Vega\\Desktop\\Progra\\Usuarios.json"));
+    for (int i =0; i<arregloJson.size();i++){
+      JSONObject elemento = (JSONObject) arregloJson.get(i);
+      Usuario usuario= new Usuario(elemento.get("nombre").toString(),elemento.get("departamento").toString(),
+      elemento.get("nombreUsuario").toString(),Integer.parseInt(elemento.get("telefono").toString()),
+      elemento.get("contraseña").toString());
+      usarios.add(usuario);
     }
   }
 }
@@ -59,16 +93,3 @@ public class Json {
     return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(obj);
 }
 }*/
-
-
-  //public boolean buscarPlaca(String pPlaca){
-      
-    //for (int i=0; i<vehiculos.size();i++){
-      //System.out.println("hola");
-      //if((vehiculos.get(i).getPlaca()).equals(pPlaca)){
-        //return false;
-        //}
-    //}
-    //return true;
-  //}
-//}
