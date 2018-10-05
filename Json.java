c59df19d4a1140a03b74c511c1dac1a1392540ff
CarlosVNZ <43708929+CarlosVNZ@users.noreply.github.com>
2018-10-05 01:04:29 -0600

    Update Json.java

--- Json.java
+++ Json.java
@@ -3,6 +3,7 @@ package logicadenegocios;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -13,13 +14,13 @@ public class Json {
   public void leerJsonVehiculo(){
     JSONParser parser= new JSONParser();
     try{
-      JSONArray arregloJson = (JSONArray) parser.parse(new FileReader("C:\\Users\\Carlos Vega\\Desktop\\Progra\\Vehiculo.json"));
+      JSONArray arregloJson = (JSONArray) parser.parse(new FileReader("Vehiculo.json"));
       for (int i =0; i<arregloJson.size();i++){
         JSONObject elemento = (JSONObject) arregloJson.get(i);
         Vehiculo vehiculo = new Vehiculo(elemento.get("placa").toString(),elemento.get("anio").toString(),elemento.get("color").toString(),
         elemento.get("marca").toString(),Integer.parseInt(elemento.get("capacidad").toString()),elemento.get("sede").toString(),
         elemento.get("estado").toString(),Integer.parseInt(elemento.get("vin").toString()));
-        Vehiculo.setVehiculo(vehiculo);
+        Usuario.setVehiculo(vehiculo);
       }
     }catch(FileNotFoundException e){
        //advertencia
@@ -30,15 +31,18 @@ public class Json {
     }
   }
   
+  
   public void leerJsonChofer(){
     JSONParser parser= new JSONParser();
     try{
-      JSONArray arregloJson = (JSONArray) parser.parse(new FileReader("C:\\Users\\Carlos Vega\\Desktop\\Progra\\Chofer.json"));
+      JSONArray arregloJson = (JSONArray) parser.parse(new FileReader("Chofer.json"));
       for (int i =0; i<arregloJson.size();i++){
         JSONObject elemento = (JSONObject) arregloJson.get(i);
         Chofer chofer= new Chofer(elemento.get("nombre").toString(),Integer.parseInt(elemento.get("cedula").toString()),
-        elemento.get("correo").toString(),Integer.parseInt(elemento.get("telefono").toString()));
-        Chofer.setChoferes(chofer);
+        elemento.get("correo").toString(),Integer.parseInt(elemento.get("telefono").toString()),
+        elemento.get("numero").toString(),elemento.get("tipo").toString(),elemento.get("fechaEmision").toString(),
+        elemento.get("fechaExpira").toString());
+        Usuario.setChoferes(chofer);
       }
     }catch(FileNotFoundException e){
        //advertencia
@@ -48,16 +52,16 @@ public class Json {
        //advertencia
     }
   }
+ 
   public void leerJsonPasajeros() {
     JSONParser parser= new JSONParser();
     try{
-      JSONArray arregloJson = (JSONArray) parser.parse(new FileReader("C:\\Users\\Carlos Vega\\Desktop\\Progra\\Pasajeros.json"));
+      JSONArray arregloJson = (JSONArray) parser.parse(new FileReader("Pasajeros.json"));
       for (int i =0; i<arregloJson.size();i++){
         JSONObject elemento = (JSONObject) arregloJson.get(i);
-        // if(buscarPlaca(elemento.get("placa").toString())){
         Pasajero pasajero= new Pasajero(elemento.get("nombre").toString(),Integer.parseInt(elemento.get("cedula").toString()),
         elemento.get("correo").toString(),Integer.parseInt(elemento.get("telefono").toString()));
-        Pasajero.setPasajeros(pasajero);
+        Usuario.setPasajeros(pasajero);
       }
     }catch(FileNotFoundException e){
        //advertencia
@@ -70,13 +74,13 @@ public class Json {
   public void leerJsonUsuarios(){
     JSONParser parser= new JSONParser();
     try{
-      JSONArray arregloJson = (JSONArray) parser.parse(new FileReader("C:\\Users\\Carlos Vega\\Desktop\\Progra\\Usuarios.json"));
+      JSONArray arregloJson = (JSONArray) parser.parse(new FileReader("Usuarios.json"));
       for (int i =0; i<arregloJson.size();i++){
         JSONObject elemento = (JSONObject) arregloJson.get(i);
-        Usuario usuario= new Usuario(elemento.get("nombre").toString(),elemento.get("departamento").toString(),
+        Secretaria secretaria= new Secretaria(elemento.get("nombre").toString(),elemento.get("departamento").toString(),
         elemento.get("nombreUsuario").toString(),Integer.parseInt(elemento.get("telefono").toString()),
         elemento.get("contraseña").toString());
-        //Usuario.setUsuario(usuario);
+        Usuario.setSecretaria(secretaria);
       }
     }catch(FileNotFoundException e){
        //advertencia
@@ -86,4 +90,5 @@ public class Json {
        //advertencia
     }
   }
+ 
 }
